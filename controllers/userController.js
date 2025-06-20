d26408aa0470a5bf6e8adf044e27f093fbef445f
amanpsharma <14mcaaman@gmail.com>
2025-06-20 11:46:48 +0530

    Add user management and product image upload

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+
+export const getUsers = async (req, res) => {
+  const users = await User.find().select("-password");
+  res.json(users);
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json({ message: "User deleted" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
